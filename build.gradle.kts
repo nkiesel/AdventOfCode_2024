4bbd5ebbfb2a00fae784c88d6de5324ed8cf2a2b
Norbert Kiesel <nk@iname.com>
2024-11-27 17:18:23 -0800

    upgrade to Kotlin 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
     alias(libs.plugins.versions)
     alias(libs.plugins.versions.filter)
     alias(libs.plugins.versions.update)
