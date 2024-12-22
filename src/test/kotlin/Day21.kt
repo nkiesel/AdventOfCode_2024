43bde29fd108d72a69e6963e212a65062e782d94
Norbert Kiesel <nk@iname.com>
2024-12-21 16:29:15 -0800

    day 21 p2

--- src/test/kotlin/Day21.kt
+++ src/test/kotlin/Day21.kt
@@ -1,5 +1,5 @@
+import io.kotest.matchers.longs.shouldBeInRange
 import io.kotest.matchers.shouldBe
-import jdk.internal.org.jline.utils.Colors.h
 import org.junit.jupiter.api.Test
 import kotlin.math.abs
 
@@ -11,6 +11,9 @@ class Day21 {
         456A
         379A
     """.trimIndent().lines()
+
+    val memory2l = mutableMapOf<Pair<Char, Char>, List<String>>()
+    val memory2 = mutableMapOf<Pair<Char, Char>, String>()
 
     private fun parse(input: List<String>) = input
 
@@ -59,21 +62,74 @@ class Day21 {
         }.map { it + "A" }
     }
 
-    private fun robot2(from: Char, to: Char): String {
-        var fr = row2(from)
-        var fc = col2(from)
-        val tr = row2(to)
-        val tc = col2(to)
-        return buildString {
-            if (fc == tc + 1 && (fc != 2)) {
-                append("<")
-                fc--
-            }
-            if (fc < tc) append(">".repeat(tc - fc))
-            if (fr > tr) append("v")
-            if (fc > tc) append("<".repeat(fc - tc))
-            if (fr < tr) append("^")
-            append("A")
+    private fun robot2(fromTo: Pair<Char, Char>): String {
+        return memory2.getOrPut(fromTo) {
+            val (from, to) = fromTo
+            var fr = row2(from)
+            var fc = col2(from)
+            val tr = row2(to)
+            val tc = col2(to)
+            val h = (if (fc < tc) ">" else "<").repeat(abs(fc - tc))
+            val v = (if (fr < tr) "^" else "v")
+            return buildString {
+                if (from != to) {
+                    if (fc == tc) {
+                        append(v)
+                    } else if (fr == tr) {
+                        append(h)
+                    } else {
+                        if (tc == 1) append(v + h) else append(h + v)
+                    }
+//                if (fc == tc + 1 && (fc != 2)) {
+//                    append("<")
+//                    fc--
+//                }
+//                if (fc < tc) append(">".repeat(tc - fc))
+//                if (fr > tr) append("v")
+//                if (fc > tc) append("<".repeat(fc - tc))
+//                if (fr < tr) append("^")
+                }
+            } + "A"
+        }
+    }
+
+    private fun robot2l(fromTo: Pair<Char, Char>): List<String> {
+        return memory2l.getOrPut(fromTo) {
+            val (from, to) = fromTo
+            var fr = row2(from)
+            var fc = col2(from)
+            val tr = row2(to)
+            val tc = col2(to)
+            val h = (if (fc < tc) ">" else "<").repeat(abs(fc - tc))
+            val v = (if (fr < tr) "^" else "v")
+            return buildList {
+                if (from == to) {
+                    add("")
+                } else {
+                    if (fc == tc) {
+                        add(v)
+                    } else if (fr == tr) {
+                        add(h)
+                    } else if (from == '<') {
+                        if (tc == 2) {
+                            add(">^")
+                        } else {
+                            add(">^>")
+                            add(">>^")
+                        }
+                    } else if (to == '<') {
+                        if (fc == 2) {
+                            add("v<")
+                        } else {
+                            add("<v<")
+                            add("v<<")
+                        }
+                    } else {
+                        add(h + v)
+                        add(v + h)
+                    }
+                }
+            }.map { it + "A" }
         }
     }
 
@@ -108,10 +164,33 @@ class Day21 {
     }
 
     private fun seq(typing: String): String {
-        val e1 = "<A^A>^^AvvvA"
-        val r1l = ("A" + typing).zipWithNext().map { c -> robot1(c.first, c.second) }
-        val per = r1l.map { it.indices }
-        val r1 = r1l.joinToString("") { it.first() }
+        val r1l = ("A$typing").zipWithNext().map { c -> robot1(c.first, c.second) }
+        val r1s = buildList {
+            for (i0 in r1l[0]) {
+                for (i1 in r1l[1]) {
+                    for (i2 in r1l[2]) {
+                        for (i3 in r1l[3]) {
+                            add(listOf(i0, i1, i2, i3).joinToString(""))
+                        }
+                    }
+                }
+            }
+        }.distinct()
+        val r2s = r1s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+        val r3s = r2s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+        if (typing == "029A") {
+            println("$typing: ${r2s.map(String::length)}")
+            println("$typing: ${r3s.map(String::length)}")
+            val r4s = r3s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+            println("$typing: ${r4s.map(String::length)}")
+            val r5s = r4s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+            println("$typing: ${r5s.map(String::length)}")
+        }
+        return r3s.minBy(String::length)
+    }
+
+    private fun seq2(typing: String, rep: Int): String {
+        val r1l = ("A$typing").zipWithNext().map { c -> robot1(c.first, c.second) }
         val r1s = buildList {
             for (i0 in r1l[0]) {
                 for (i1 in r1l[1]) {
@@ -123,19 +202,59 @@ class Day21 {
                 }
             }
         }
-//        val r1 = ("A" + typing).zipWithNext().joinToString("") { c -> robot1(c.first, c.second) }
-        val o1 = e1 == r1
-        val e2 = "v<<A>>^A<A>AvA<^AA>A<vAAA>^A"
-        val r2 = ("A" + r1).zipWithNext().joinToString("") { c -> robot2(c.first, c.second) }
-        val r2s = r1s.map { ("A" + it).zipWithNext().joinToString("") { c -> robot2(c.first, c.second) }}
+        var r2s = r1s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+        repeat(rep - 1) {
+            r2s = r2s.map { ("A$it").zipWithNext().joinToString("") { robot2(it) } }.distinct()
+        }
+        return r2s.minBy(String::length)
+    }
 
-        val o2 = e2 == r2
-        val e3 = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
-        val r3 = ("A" + r2).zipWithNext().joinToString("") { c -> robot3(c.first, c.second) }
-        val r3s = r2s.map { ("A" + it).zipWithNext().joinToString("") { c -> robot2(c.first, c.second) }}
-        val o3 = e3 == r3
-        log(typing, r3, r2, r1)
-        return r3s.minBy(String::length)
+    fun combi(r: List<List<String>>): List<String> {
+        var rs = listOf<String>("")
+        r.forEach { l ->
+            val nl = l.map { a -> rs.map { it + a } }
+            rs = nl.flatten()
+        }
+        return rs.distinct()
+    }
+
+    @Test
+    fun ct() {
+        val l = listOf(listOf("A"), listOf("B1", "B2"), listOf("C"), listOf("D1", "D2"))
+        val c = combi(l)
+        c.size shouldBe 4
+    }
+
+    private fun steps(ft: Pair<Char, Char>): Int = steps(ft.first, ft.second)
+
+    private fun steps(f: Char, t: Char): Int {
+        return (row2(f) delta row2(t)) + (col2(f) delta col2(t))
+    }
+
+    private fun seq2a(typing: String, rep: Int): Long {
+        return combi(("A$typing").zipWithNext().map { c -> robot1(c.first, c.second) }).minOf { r1 ->
+            combi("A$r1".zipWithNext().map { robot2l(it) }).minOf { nk ->
+                var cm = CountingMap(("A$nk").zipWithNext())
+                repeat(rep - 1) {
+                    val n = CountingMap<Pair<Char, Char>>()
+                    for ((p, v) in cm.entries()) {
+                        val robot2 = robot2l(p).minBy { r ->
+                            "${r}A".zipWithNext().flatMap {
+                                robot2l(it).map {
+                                    it.zipWithNext().sumOf { steps(it) }
+                                }
+                            }.sum()
+                        }
+                        n.inc('A' to robot2.first(), v)
+                        robot2.zipWithNext().forEach {
+                            n.inc(it, v)
+                        }
+                    }
+                    cm = n
+                }
+                cm.values().sum()
+            }
+        }
     }
 
     private fun reverse1(s: String): String {
@@ -229,25 +348,35 @@ class Day21 {
             val l = s.length
             val n = line.ints().first()
             val r = l * n
-            println("$line -> $s -> $l * $n -> $r")
+//            println("$line -> $s -> $l * $n -> $r")
             r
         }
     }
 
-    private fun two(input: List<String>): Int {
-        return 0
+    private fun two(input: List<String>, rep: Int): Long {
+        return input.sumOf { line ->
+            val l = seq2a(line, rep)
+            val n = line.ints().first()
+            val r = l * n
+            println("$line -> $l * $n -> $r")
+            r
+        }.toLong()
     }
 
     @Test
     fun testOne(input: List<String>) {
         one(sample) shouldBe 126384
-        one(input) shouldBe 0
+        one(input) shouldBe 278748
     }
 
     @Test
     fun testTwo(input: List<String>) {
-//        two(sample) shouldBe 0
-//        two(input) shouldBe 0
+        two(sample, 2) shouldBe 126384L
+//        two(sample, 4) shouldBe 126384L
+        two(input, 2) shouldBe 278748L
+        val two = two(input, 25)
+        two shouldBeInRange 38963892L..<381421277177524L
+        println(two)
     }
 }
 
